89edc654c480f61c5a70e855d3de7c6c60ea9a6e
danilodanilo1 <danilo.onascimento@hotmail.com>
2020-06-18 16:40:54 -0300

    api rest completa com node

--- aula01/app.js
+++
@@ -1,5 +0,0 @@
-const m1 = require('./mod1');
-const m2 = require('./mod2');
-
-m1('carregando m1')
-m2.mensagem('mensaaagem numero 2')
--- aula01/mod2.js
+++
@@ -1,3 +0,0 @@
-exports.mensagem = function (x) {
-  console.log(x)
-}
